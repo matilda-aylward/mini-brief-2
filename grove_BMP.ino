d824bc1106e2969aa0e224c804c979d6a9af9619
matilda-aylward <matilda.aylward@hotmail.com>
2026-01-08 17:28:02 +0000

    Add files via upload

---
+++ grove_BMP.ino
@@ -0,0 +1,41 @@
+//this code is heavily based off of the PulseSensorPlayground example "Getting_BPM_to_Monitor"
+//only minor tweeks have been made to send just the bpm values 
+
+//this code should be ran off 3 seperate computers with a grove ear-clip in each.
+// they will all be connected to the serial brige in one laptop
+#include <PulseSensorPlayground.h>
+
+
+const int PulseWire = 0;
+const int LED = LED_BUILTIN;
+int Threshold = 550;
+
+PulseSensorPlayground pulseSensor;
+
+void setup() {
+ 
+Serial.begin(115200); 
+pulseSensor.analogInput(PulseWire);
+pulseSensor.blinkOnPulse(LED);
+pulseSensor.setThreshold(Threshold);
+
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  
+  }
+}
+
+
+
+void loop() {
+ 
+
+if (pulseSensor.sawStartOfBeat()) {           
+int myBPM = pulseSensor.getBeatsPerMinute();
+
+ Serial.println(myBPM); 
+
+}
+
+  delay(20);
+
+}
